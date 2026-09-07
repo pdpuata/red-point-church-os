44b903aa39832273615a4c5a27fda3cc5ab93d28
pdpuata <pdpuata@gmail.com>
2026-09-07 16:19:57 +0200

    feat: surface AI operating model in church OS

--- src/os/PeopleCapabilityOS.tsx
+++ src/os/PeopleCapabilityOS.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import BandsOS from './BandsOS';
 import PeopleCapabilityLegacyOS from './PeopleCapabilityLegacyOS';
 import SongSelectMusicOS from './SongSelectMusicOS';
+import AIOperatingModelOS from './AIOperatingModelOS';
 
 export default function PeopleCapabilityOS() {
   return <>
+    <AIOperatingModelOS />
     <SongSelectMusicOS />
     <BandsOS />
     <PeopleCapabilityLegacyOS />
