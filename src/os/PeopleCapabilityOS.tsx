0d0edc9aa0d5fe8e83d2fe04c13049d02d515e0b
pdpuata <pdpuata@gmail.com>
2026-09-07 15:49:45 +0200

    feat: surface SongSelect music library in Church OS

--- src/os/PeopleCapabilityOS.tsx
+++ src/os/PeopleCapabilityOS.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import BandsOS from './BandsOS';
 import PeopleCapabilityLegacyOS from './PeopleCapabilityLegacyOS';
+import SongSelectMusicOS from './SongSelectMusicOS';
 
 export default function PeopleCapabilityOS() {
   return <>
+    <SongSelectMusicOS />
     <BandsOS />
     <PeopleCapabilityLegacyOS />
   </>;
